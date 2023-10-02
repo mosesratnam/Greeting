f9da248d528fde753eb2b6eddc75ff3873fe71f4
Mohan Ratnam <mosesratnam@yahoo.com>
2023-10-02 12:05:37 +0100

    A c# greeting program

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Greeting;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        #nullable disable
+        Console.WriteLine("Greeting!");
+        Console.Write("Please type your first name?");
+        string userName = Console.ReadLine();
+        Console.WriteLine("Hello {0}, nice to meet you!", userName);
+    }
+}
