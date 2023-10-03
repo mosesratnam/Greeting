8f4a13222a50f34e627505a8f317547a81ed4d5c
Mohan Ratnam <mosesratnam@yahoo.com>
2023-10-03 09:41:23 +0100

    https://github.com/mosesratnam/Greeting.git

--- Program.cs
+++ Program.cs
@@ -6,8 +6,21 @@ class Program
     {
         #nullable disable
         Console.WriteLine("Greeting!");
+        
         Console.Write("Please type your first name?");
-        string userName = Console.ReadLine();
-        Console.WriteLine("Hello {0}, nice to meet you!", userName);
+        string firstName = Console.ReadLine();
+        
+        Console.Write("Please type your last name?");
+        string lastName = Console.ReadLine();
+        
+        if(firstName != "" & lastName != ""){
+            Console.WriteLine("Hello {0} {1}!", firstName, lastName);
+        } else if(firstName == "" & lastName == ""){
+            Console.WriteLine("I see you're not cooperating!");            
+        } else if(firstName == ""){
+            Console.WriteLine("Hello {0}, I see you don't have a first name!", lastName);
+        } else if(lastName == ""){
+            Console.WriteLine("Hello {0}, I see you don't have a last name!", firstName);
+        }
     }
 }
