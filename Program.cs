523d74d1550ab746e9a252a0fd74db0c08731002
Mohan Ratnam <mosesratnam@yahoo.com>
2023-10-05 16:36:47 +0100

    Modified to add Person class and methods for variius checks

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Greeting;
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Greeting;
 
 class Program
 {
@@ -6,52 +8,42 @@ class Program
     {
         #nullable disable
         Console.WriteLine("Greeting!");
-        
+
+        Person user = new Person();        
         Console.Write("Please type your first name?");
-        string firstName = Console.ReadLine();
+        user.firstName = Console.ReadLine();
                 
         Console.Write("Please type your last name?");
-        string lastName = Console.ReadLine();
+        user.lastName = Console.ReadLine();
  
         Console.Write("Please enter your birthdate?");
-        DateTime birthdate = DateTime.Parse(Console.ReadLine());
-        int userAge = DateTime.Today.Year - birthdate.Year;
-        if(DateTime.Today.Year - birthdate.Year < 10){
-            Console.WriteLine("Do you like dinosaurs?");
+        user.birthDate = DateTime.Parse(Console.ReadLine());
+        if (user.IsUnderTen()){
+            Console.WriteLine("You must like donosaurs!");
         } else {
-            Console.WriteLine("You're a big boy now!");
+            Console.WriteLine("You're a big boy!");
         }
-        //TimeSpan userAge = DateTime.Today.Year - birthdate.Year;
-        //Console.WriteLine(userAge.Ticks);
-
-        if(firstName != "" & lastName != ""){
-            if(CheckIsUpper(true, firstName) & CheckIsUpper(true, lastName)){
-                Console.WriteLine("Hello {0} {1}! no need to shout!", firstName.ToLower(), lastName.ToLower());  
+        
+        
+        if(user.firstName != "" & user.lastName != ""){
+            if(user.IsNameUpper()){
+                Console.WriteLine("Hello {0} {1}! no need to shout!", user.firstName.ToLower(), user.lastName.ToLower()); 
             } else {
-                Console.WriteLine("Hello {0} {1}!", firstName, lastName);
-            }    
-            if(firstName.Length + lastName.Length > 12){
+                Console.WriteLine(user.Greeting());
+            }
+            
+            if(user.IsLongName()){
                 Console.WriteLine("Gosh, you have a long name!");
-            }       
-        } else if(firstName == "" & lastName == ""){
+            }    
+
+        } else if(user.firstName == "" & user.lastName == ""){
             Console.WriteLine("I see you're not cooperating!");            
-        } else if(firstName == ""){
-            Console.WriteLine("Hello {0}, I see you don't have a first name!", lastName);
-        } else if(lastName == ""){
-            Console.WriteLine("Hello {0}, I see you don't have a last name!", firstName);
+        } else if(user.firstName == ""){
+            Console.WriteLine("Hello {0}, I see you don't have a first name!", user.lastName);
+        } else if(user.lastName == ""){
+            Console.WriteLine("Hello {0}, I see you don't have a last name!", user.firstName);
         }
     }
-
-    /* bool isUpper = true;
-            int i = 0;
-            while (isUpper & i < firstName.Length)
-            {
-                isUpper = char.IsUpper(firstName[i]);
-                i++;                
-            }
-            if (isUpper){
-                Console.WriteLine("no need to shout!");
-            } */
 
     private static bool CheckIsUpper(bool upper, string name){
         for (int i = 0; i < name.Length; i++)
@@ -63,6 +55,47 @@ class Program
         }
         return upper;
     }  
+}
 
-     
+class Person
+{
+    public string firstName { get; set; }
+    public string lastName { get; set; }
+    public DateTime birthDate { get; set; } 
+    public string Greeting(){
+        return String.Format("Hello, {0} {1}!", firstName, lastName);
+    }
+    public bool IsUnderTen(){
+        return (DateTime.Today.Year - birthDate.Year) < 9;        
+    }
+
+    public bool IsLongName(){
+        return (firstName.Length + lastName.Length) > 12;
+    } 
+
+    public bool IsNameUpper(){
+        bool upper = true;
+
+        for (int i = 0; i < firstName.Length; i++)
+        {
+            if(!char.IsUpper(firstName[i])){
+                upper = false;
+                break;
+            }
+        }
+
+        if(upper){
+            for (int i = 0; i < lastName.Length; i++)
+            {
+                if(!char.IsUpper(lastName[i])){
+                    upper = false;
+                    break;
+                }
+            }
+        } else {
+            return upper;
+        }
+
+        return upper;  
+    } 
 }
